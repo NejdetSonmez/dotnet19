76a38c434150ef3722f56ea67e0039556a8bb89b
NejdetSonmez <mustafanejdetsonmez@gmail.com>
2019-01-15 19:40:50 +0300

    Powerball random number

---
+++ Powerball/Powerball/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Powerball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("POWERBALL");
+            var powerball = new Powerball();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(powerball.generateNumbers());
+                Console.ReadKey();
+            }
+        }
+    }
+    class Powerball
+    {
+        public string generateNumbers()
+        {
+            string numbers = "";
+            Random rnd = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                int num = rnd.Next(1, 56);
+                numbers += num.ToString() + " ";
+            }
+            return numbers;
+        }
+       
+    }
+    
+}
